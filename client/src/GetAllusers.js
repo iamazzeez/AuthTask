56a96dc0f3e7486ab69433b22ed2b05d064c9e94
Abdul Azeez <abdaseez@gmail.com>
2019-06-14 22:50:50 +0530

    card view

--- client/src/GetAllusers.js
+++ client/src/GetAllusers.js
@@ -43,11 +43,23 @@ export default class GetAllusers extends Component {
                 <div className='dispaly-4 my-3 '>
                      {Object.values(this.state.data).map(user => (   
                     <div className="container"style={{maxWidth: '25rem'}} >
-                    <h5>Username: {user.username}</h5>
+                    {/* <h5>Username: {user.username}</h5>
                     <h5>Name: {user.name}</h5>
                     <h5>Email: {user.email}</h5>
                     <h5>Password: {user.password}</h5>
-                    <h5>Verified: {`${user.isValid}`}</h5>
+                    <h5>Verified: {`${user.isValid}`}</h5> */}
+                    <div class="card">
+  <div class="card-body">
+    <h4 class="card-title">{user.name}</h4>
+    <h6 class="card-subtitle mb-2 text-muted">{user.username}</h6>
+    <p class="card-text">Email: {user.email}</p>
+    <p class="card-text">Password: {user.password}</p>
+    <p class="card-text">Validated: {`${user.isValid}`}</p>
+    <a href="#" class="card-link">Validate</a>
+    <a href="#" class="card-link">Delete</a>
+    <a href="#" class="card-link">Edit</a>
+  </div>
+</div>
                     <br />
                     </div>
                     ))}
